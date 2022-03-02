50d51bfcbb61a25cc7bc153b41a0f117a411e876
bluebayit7811 <bluebayitltd@gmail.com>
2022-03-02 18:39:17 +0600

    added product discount in new arrival

--- src/components/product-cards/ProductCard2.tsx
+++ src/components/product-cards/ProductCard2.tsx
@@ -1,9 +1,12 @@
+import { Chip } from "@component/Chip";
 import Currency from "@component/Currency";
 import HoverBox from "@component/HoverBox";
 import LazyImage from "@component/LazyImage";
 import { H4 } from "@component/Typography";
+import { Product_Discount_By_Id } from "@data/constants";
+import axios from "axios";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 export interface ProductCard2Props {
   imgUrl: string;
@@ -11,6 +14,7 @@ export interface ProductCard2Props {
   price: number;
   productUrl: string;
   condition: string;
+  id: number;
 }
 
 const ProductCard2: React.FC<ProductCard2Props> = ({
@@ -18,39 +22,78 @@ const ProductCard2: React.FC<ProductCard2Props> = ({
   title,
   price,
   productUrl,
-  condition
+  condition,
+  id
 }) => {
+  const [orginalPrice, setorginalPrice] = useState(0)
+  const [discountedPercent, setdiscountedPercent] = useState(0)
+
+  useEffect(() => {
+    axios.get(`${Product_Discount_By_Id}${id}`).then(res => {
+      console.log("descountRes", res)
+      if (res.data.discounts?.discounted_price) {
+        setorginalPrice(Number(res.data.discounts?.product.unit_price))
+        setdiscountedPercent(res.data.discounts?.discount_percent)
+      }
+    })
+  }, [])
   return (
-    <Link href={productUrl}>
-      <a>
-        <HoverBox borderRadius={8} mb="0.5rem">
-          <LazyImage
-            src={imgUrl}
-            loader={() =>
-              imgUrl
-            }
-            width="100%"
-            height="auto"
-            layout="responsive"
-            alt={title}
-          />
-        </HoverBox>
-        <H4 fontWeight="600" fontSize="14px" mb="0.25rem">
-          {title}
-        </H4>
-        <H4 fontWeight="600" fontSize="14px" color="primary.main">
-          <Currency>{Math.ceil(price).toLocaleString()}</Currency>
-        </H4>
-        <H4
-          display="flex"
-          className="title"
-          fontSize="13px"
+    <>
+      {!!discountedPercent && (
+        <Chip
+          position="absolute"
+          bg="primary.main"
+          color="primary.text"
+          fontSize="10px"
           fontWeight="600"
-          color={(condition === "new" || condition === "New" || condition === "NEW") ? "primary.main" : "secondary.main"}
-        >{condition || ""}
-        </H4>
-      </a>
-    </Link>
+          p="5px 10px"
+          top="10px"
+          left="10px"
+          zIndex={2}
+        >
+          {discountedPercent}% off
+        </Chip>
+      )}
+      <Link href={productUrl}>
+        <a>
+          <HoverBox borderRadius={8} mb="0.5rem">
+            <LazyImage
+              src={imgUrl}
+              loader={() =>
+                imgUrl
+              }
+              width="100%"
+              height="auto"
+              layout="responsive"
+              alt={title}
+            />
+          </HoverBox>
+          <H4 fontWeight="600" fontSize="14px" mb="0.25rem">
+            {title}
+          </H4>
+
+          <div style={{ display: 'flex' }}>
+            {!!orginalPrice && (
+              <H4 fontWeight="600" fontSize="14px" color="text.muted" style={{ marginRight: '5px' }}>
+                <del><Currency>{orginalPrice}</Currency></del>
+              </H4>
+            )}
+            <H4 fontWeight="600" fontSize="14px" color="primary.main">
+              <Currency>{Number(price).toFixed(2)}</Currency>
+            </H4>
+          </div>
+          {/* <Currency>{Math.ceil(price).toLocaleString()}</Currency> */}
+          <H4
+            display="flex"
+            className="title"
+            fontSize="13px"
+            fontWeight="600"
+            color={(condition === "new" || condition === "New" || condition === "NEW") ? "primary.main" : "secondary.main"}
+          >{condition || ""}
+          </H4>
+        </a>
+      </Link >
+    </>
   );
 };
 
